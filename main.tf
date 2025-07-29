ed8b59c2a4e19912a32dc324c14a367be4a967c7
austinkaruru <karuruaustine@gmail.com>
2025-07-29 21:48:19 +0300

    finalized on creating jumpbox vm and its requirements

--- main.tf
+++ main.tf
@@ -1,30 +1,80 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
   credentials = var.google_credentials
 }
 
+resource "google_compute_project_metadata" "my_ssh_key" {
+  metadata = {
+    ssh-keys = "${var.ssh_username}:${file(var.ssh_key)}"
+  }
+}
+resource "google_compute_address" "jumpbox_ip" {
+  name   = "${var.jumpbox_name}-ip"
+  region = var.region
+}
+
+data "google_compute_image" "debian" {
+  family  = "debian-12"
+  project = "debian-cloud"
+}
+
+data "google_service_account" "tf_sa" {
+  account_id = var.sa_account
+}
 resource "google_compute_network" "tf_network" {
   name                    = var.network_name
   auto_create_subnetworks = true
+
 }
 
-resource "google_compute_instance" "jumpbox" {
-  name         = var.jumpbox_name
-  machine_type = var.machine_type
-  zone         = var.zone
+resource "google_compute_firewall" "k8s_firewall" {
+  name    = "${var.network_name}-firewall"
+  network = google_compute_network.tf_network.id
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+  target_tags   = ["k8s-thw"]
+  source_ranges = ["0.0.0.0/0"]
+}
 
+resource "google_compute_disk" "jumpbox_disk" {
+  name  = "${var.jumpbox_name}-disk"
+  type  = "pd-standard"
+  image = data.google_compute_image.debian.self_link
+  zone  = var.zone
+  size  = 10
+}
+resource "google_compute_instance" "jumpbox" {
+  name                      = var.jumpbox_name
+  machine_type              = var.machine_type
+  zone                      = var.zone
+  tags                      = ["k8s-thw"]
+  allow_stopping_for_update = true
   boot_disk {
-    initialize_params {
-      image = var.image
-    }
+    source      = google_compute_disk.jumpbox_disk.id
+    auto_delete = true
   }
 
   network_interface {
     network = google_compute_network.tf_network.id
     access_config {
-
+      nat_ip = google_compute_address.jumpbox_ip.address
     }
   }
+  service_account {
+    email  = data.google_service_account.tf_sa.email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+}
+
+output "jumpbox_ip" {
+  value = google_compute_address.jumpbox_ip.address
+}
+
+output "tf_sa_account" {
+  value = data.google_service_account.tf_sa.email
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,45 @@
-variable "project_id" {}
-variable "region" {}
-variable "zone" {}
-variable "jumpbox_name" {}
-variable "machine_type" {}
-variable "image" {}
-variable "network_name" {}
+variable "project_id" {
+  description = "The Google Cloud project ID where resources will be created."
+  type        = string
+}
+
+variable "ssh_key" {
+  description = "Path to the SSH public key file to be added to the project metadata."
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_username" {
+  description = "The username for the SSH key."
+  type        = string
+}
+variable "region" {
+  description = "The Google Cloud region where resources will be created."
+  default     = "europe-west4"
+}
+variable "zone" {
+  description = "The Google Cloud zone where resources will be created."
+  default     = "europe-west4-a"
+}
+variable "jumpbox_name" {
+  description = "The name of the jumpbox instance."
+  default     = "jumpbox"
+}
+variable "machine_type" {
+  description = "The machine type for the instances."
+  default     = "e2-medium"
+}
+variable "network_name" {
+  description = "The name of the network to create."
+}
 variable "google_credentials" {
-    type = string
-    description = "Path to the Google Cloud service account credentials JSON file."
-    sensitive = true
+  type        = string
+  description = "Path to the Google Cloud service account credentials JSON file."
+  sensitive   = true
+  default     = "credentials/k8s-tf.json"
+}
+
+variable "sa_account" {
+  description = "The service account to use for the Google Cloud resources."
+  type        = string
 }
